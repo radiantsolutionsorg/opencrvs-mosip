1d66840ac1cd2d996aceedba99142dd2318b0f49
naftis <pyry.rouvila@gmail.com>
2024-02-08 11:02:14 +0200

    chore: edit address translations to IND/MEC instead of default FAR

--- src/form/addresses/address-fields.ts
+++ src/form/addresses/address-fields.ts
@@ -267,7 +267,7 @@ export function getAddressFields(
         ? useCase
         : `${useCase}Address`,
       required: true,
-      initialValue: 'FAR',
+      initialValue: 'IND',
       validator: [],
       placeholder: {
         defaultMessage: 'Select',
